dc79f6e19bd34b081c0cacb22c39b188578de26e
Shayne Meyer <shayne.meyer@gmail.com>
2022-08-30 18:54:34 -0700

    added events/me endpoint to return events by current user

--- dj-events-backend/src/api/event/controllers/event.js
+++ dj-events-backend/src/api/event/controllers/event.js
@@ -6,9 +6,28 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::event.event", {
+module.exports = createCoreController("api::event.event", ({ strapi }) => ({
   count(ctx) {
-    var { query } = ctx.request;
+    const { query } = ctx.request;
     return strapi.query("api::event.event").count({ where: query });
   },
-});
+  async me(ctx) {
+    const user = ctx.state.user;
+    if (!user) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] },
+      ]);
+    }
+    const query = {
+      filters: {
+        user: { id: user.id },
+      },
+    };
+    const data = await this.find({ query: query });
+    if (!data) {
+      return ctx.notFound();
+    }
+    const sanitizedEntity = await this.sanitizeOutput(data, ctx);
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
---
+++ dj-events-backend/src/api/event/routes/custom-event.js
@@ -0,0 +1,9 @@
+module.exports = {
+  routes: [
+    {
+      method: "GET",
+      path: "/events/me",
+      handler: "event.me",
+    },
+  ],
+};
